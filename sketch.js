68b7f9de72beaa62c7700482e969908afded5b79
AlejandroK7vell <alexk7v@gmail.com>
2022-07-10 08:52:16 -0500

    commit

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function draw() {
   tint(...colors[currentColor]);
   //image(heart, getRandomInt(-100,Wwidth-50), getRandomInt(-100, wHeight-50));
   
-  image(img, Wwidth/2 - 256, wHeight/2 - 256);
+  image(imagen, Wwidth/2 - 284, wHeight/2 - 213);
   fill(0,0,0);
   text('Te Amo Vanessita', Wwidth/2, 95);
   text('Te Amo', txtX, wHeight - 100);
